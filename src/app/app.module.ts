ad7c37e91f5456fc61f07019a9a050460f77a6d3
Serhii Verbitskiy <sjcris.me@gmail.com>
2017-05-03 21:10:39 +0300

    empty project with material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { ProductCartComponent } from './product-cart/product-cart.component';
+import { FilmCardComponent } from './film-card/film-card.component';
+import { FilmCardService } from './services/film-card.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCartComponent,
+    FilmCardComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [FilmCardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
