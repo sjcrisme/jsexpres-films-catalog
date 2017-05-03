6ed8077540b85b76602eb432b629c9d78c162d13
Serhii Verbitskiy <sjcris.me@gmail.com>
2017-05-03 23:56:04 +0300

    add body in service, connect componetn film to main component

--- src/app/services/film-card.service.ts
+++ src/app/services/film-card.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class FilmCardService {
 
-  constructor() { }
+  url:string = "http://omdbapi.com/?s=start&page=1";
+  
+  constructor(private http:Http) { }
+
+  private extractData(res:Response){
+    let body = res.json();
+    return body.Search || {};
+  }
+  
+  getFilms(){
+    return this.http.get(this.url).map(this.extractData);
+  }
 
 }
